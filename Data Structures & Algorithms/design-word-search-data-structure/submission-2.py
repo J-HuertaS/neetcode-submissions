e30c65d0ca0d40afb964595661390882ed1c68d9
Juan HuertaS <105402313+J-HuertaS@users.noreply.github.com>
2026-09-10 22:23:56 -0500

    Add: design-word-search-data-structure - submission-2

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-2.py
@@ -0,0 +1,43 @@
+class TreeNode:
+    def __init__(self,word = False):
+        self.word = word  
+        self.children = [None] * 26
+
+
+class WordDictionary:
+    def __init__(self):
+        self.head = TreeNode()
+
+    def addWord(self, word: str) -> None:
+        curr = self.head
+        for char in word:
+            idx = ord(char)-97
+            if curr.children[idx] is None:
+                curr.children[idx] = TreeNode()
+            curr = curr.children[idx]
+        curr.word = True
+
+    def search(self, word: str) -> bool:
+        def dfs(initial_node, init_index, word):
+            curr = initial_node
+            for i in range(init_index,len(word)):
+                if word[i] != ".":
+                    idx = ord(word[i])-97
+                    if curr.children[idx] is None:
+                        return False
+                    curr = curr.children[idx]
+                else:
+                    for op in curr.children:
+                        if op is not None and dfs(op,i+1,word):
+                            return True
+                    return False
+
+            return curr.word
+        
+        return dfs(self.head,0,word)
+                    
+        
+            
+                
+
+        
